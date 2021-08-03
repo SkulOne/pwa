d1e8969f9581a97a5c6f158c0e17d193d58f14b2
Кулиев Султан <sultan.kuliev@simbirsoft.com>
2021-08-03 16:58:12 +0400

    feat: init PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
+import {WeatherClientService} from './services/weather-client.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'phoenix-weather-pwa';
+  data: any;
+
+  constructor(updates: SwUpdate, private weatherClient: WeatherClientService) {
+    updates.available.subscribe(() => {
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+  }
+
+  ngOnInit(): void {
+    this.data = this.weatherClient.getWeather();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
+import {HttpClientModule} from '@angular/common/http';
+import {WeatherClientService} from './services/weather-client.service';
 
 @NgModule({
   declarations: [
@@ -18,9 +20,11 @@ import { environment } from '../environments/environment';
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [WeatherClientService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
